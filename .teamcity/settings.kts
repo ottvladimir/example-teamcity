225ae194fc1186bf7557e20abf7c66f0b3d363a5
ottvladimir <77604827+ottvladimir@users.noreply.github.com>
2021-08-31 15:37:01 +1000

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -6,7 +6,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 /*
 The settings script is an entry point for defining a TeamCity
 project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
+project() function with a Project an init function as
 an argument.
 
 VcsRoots, BuildTypes, Templates, and subprojects can be
